9375651e46a86dadb659513bdb1d5f5b4eb1e6ab
liys <liys@eastcom-sw.com>
2020-12-10 15:16:39 +0800

    123

---
+++ Demo/Demo/Common.h
@@ -0,0 +1,17 @@
+//
+//  Common.h
+//  Demo
+//
+//  Created by dx on 2020/12/10.
+//  Copyright © 2020 CodeMa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Common : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo/Demo/Common.m
@@ -0,0 +1,21 @@
+//
+//  Common.m
+//  Demo
+//
+//  Created by dx on 2020/12/10.
+//  Copyright © 2020 CodeMa. All rights reserved.
+//
+
+#import "Common.h"
+
+@implementation Common
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
